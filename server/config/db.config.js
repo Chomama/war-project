ecbf66f7891ec649da11a0932803d169e569c0f6
Ryan Cho <ryanmcho@gmail.com>
2022-01-11 21:04:18 -0500

    adjusted config

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,7 +3,7 @@ const db = mysql.createConnection({
   host: "wardb.cajxtqi5zhhl.us-east-2.rds.amazonaws.com",
   port: "3306",
   user: "admin",
-  password: "AspenCapital123",
+  password: "",
   database: "war",
 });
 
